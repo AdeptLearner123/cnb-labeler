77f0f51fc53c9f403f1b68346f4c6df266b36082
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-12-08 22:18:43 -0800

    update

---
+++ tasks.py
@@ -0,0 +1,25 @@
+"""
+Tasks to be invoked via "poetry run".
+
+Any task added here must also be specified as a "script" in pyproject.toml.
+"""
+
+import os
+import subprocess
+import sys
+from typing import List, Union
+
+repo_root = os.path.dirname(__file__)
+
+
+def _command(command: Union[List[str], str], shell: bool = False):
+    subprocess.run(
+        command,
+        cwd=repo_root,
+        shell=shell,
+    )
+
+
+def format():
+    _command(["black", "."])
+    _command(["isort", "."])
